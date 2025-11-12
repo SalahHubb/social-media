e0c36d60ea68141d90a7db9db36eb640f6059304
Salah <mosalahkd028@gmail.com>
2025-11-12 05:29:03 -0700

    Add contect

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,20 +3,29 @@ const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
+  const [isCreateStroyModalOpen, setIsCreateStoryModalOpen] = useState(false);
+  const [bgCreateStoryModal, setBgCreateStoryModal] = useState("#4F46E5");
+  const [isEditProfileOpen, setIsEditProfileOpen] = useState(false);
+
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+  const openCreateStoryModal = () => setIsCreateStoryModalOpen(true);
+  const closeCreateStoryModal = () => setIsCreateStoryModalOpen(false);
+  const openEditProfile = () => setIsEditProfileOpen(true);
+  const closeEditProfile = () => setIsEditProfileOpen(false);
+
+  const value = {
+    isMobileMenuOpen,
+    toggleMobileMenu,
+    isCreateStroyModalOpen,
+    openCreateStoryModal,
+    closeCreateStoryModal,
+    bgCreateStoryModal,
+    setBgCreateStoryModal,
+    isEditProfileOpen,
+    openEditProfile,
+    closeEditProfile,
   };
 
-  return (
-    <AuthContext.Provider value={{ isMobileMenuOpen, toggleMobileMenu }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 export { AuthContext, AuthProvider };
-
-//   <div
-//     className={`fixed top-0 left-0 min-h-screen w-64 bg-slate-100 shadow-lg transform transition-transform duration-300 ease-in-out ${
-//       isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"
-//     } lg:hidden z-50`}
-//   ></div>
