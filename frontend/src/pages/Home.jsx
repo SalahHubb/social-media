c5f6f3b907b6b509adcd15c18eee1381a590c67f
Salah <mosalahkd028@gmail.com>
2025-11-12 05:23:19 -0700

    Add a reponsive Home  page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import Navbar from "../components/Navbar.jsx";
+import React, { useContext } from "react";
+import Navbar from "../components/NavBar.jsx";
 import Main from "../components/Main.jsx";
+import CreateStoryModal from "../components/CreateStroyModal.jsx";
+import { AuthContext } from "../context/AuthContext.jsx";
+import EditProfile from "../components/EditProfile.jsx";
 
 const Home = () => {
+  const {
+    isCreateStroyModalOpen,
+    closeCreateStoryModal,
+    isEditProfileOpen,
+    closeEditProfile,
+  } = useContext(AuthContext);
+
   return (
     <div className="w-screen h-screen flex bg-slate-50 relative">
       <Navbar />
       <Main />
+      <CreateStoryModal
+        open={isCreateStroyModalOpen}
+        onClose={closeCreateStoryModal}
+      />
+      <EditProfile open={isEditProfileOpen} onClose={closeEditProfile} />
     </div>
   );
 };
