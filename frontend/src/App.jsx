0241f12a4b78699f1db076e58d539ee54f021154
Salah <mosalahkd028@gmail.com>
2025-11-13 00:10:16 -0700

    Add a nested routes for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,40 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import Home from "./pages/Home.jsx";
+import Feed from "./pages/Feed.jsx";
+import Messages from "./pages/Messages.jsx";
+import Connections from "./pages/Connections.jsx";
+import Discover from "./pages/Discover.jsx";
+import Profile from "./pages/Profile.jsx";
+import CreatePost from "./pages/CreatePost.jsx";
+import ChatRoom from "./pages/ChatRoom.jsx";
+import Followers from "./components/Followers.jsx";
+import Following from "./components/Following.jsx";
+import Pending from "./components/Pending.jsx";
+import UserConnections from "./components/UserConnections.jsx";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path={"/"} element={<Login />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/"} element={<Home />}>
+          <Route index element={<Feed />} />
+          <Route path={"feed"} element={<Feed />} />
+          <Route path={"messages"} element={<Messages />} />
+          <Route path={"connections"} element={<Connections />} />
+          <Route path={"discover"} element={<Discover />} />
+          <Route path={"connections"} element={<Connections />}>
+            <Route path={"followers"} element={<Followers />} />
+            <Route path={"following"} element={<Following />} />
+            <Route path={"pending"} element={<Pending />} />
+            <Route path={"user-connections"} element={<UserConnections />} />
+          </Route>
+          <Route path={"profile"} element={<Profile />} />
+          <Route path={"create-post"} element={<CreatePost />} />
+          <Route path={"chat"} element={<ChatRoom />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
