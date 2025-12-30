2573a3f2a859ba26f487e6feb54ab2aa6c14e2af
Salah <mosalahkd028@gmail.com>
2025-12-30 11:51:17 -0700

    Update hardcoded frontend url by env variable

--- backend/server.js
+++ backend/server.js
@@ -16,14 +16,14 @@ import connectionRouter from "./routes/connectionRoute.js";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
+  cors: { origin: process.env.FRONTEND_URL, methods: ["GET", "POST"] },
 });
 const PORT = process.env.PORT;
 connectToDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true, // allow sending of cookies
   })
 );
