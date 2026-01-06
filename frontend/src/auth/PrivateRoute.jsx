cfce8470c11c6df535748f6db97020febe641708
Salah <mosalahkd028@gmail.com>
2026-01-06 00:55:26 -0700

    Remove useNavigate

--- frontend/src/auth/PrivateRoute.jsx
+++ frontend/src/auth/PrivateRoute.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const navigate = useNavigate();
-
   return (
     <>
       <SignedIn>{children ?? <Outlet />}</SignedIn>
